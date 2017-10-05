166520684ae2ec4b4ebdefafccab91ac735f0b88
captproton <carl@wdwhub.net>
2017-10-05 11:07:15 -0700

    WIP: new_hotel_show_page

--- spec/support/i18n.rb
+++ spec/support/i18n.rb
@@ -1,3 +1,9 @@
 RSpec.configure do |config|
   config.include ActionView::Helpers::TranslationHelper
+  en:
+  hotels:
+    index:
+      title: "WDW Hub|Walt Disney World Hotels"
+    show:
+      title: "Choose %{name}!"
 end
