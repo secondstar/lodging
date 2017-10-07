2910079696a2fabd1ae1cf8bd36c744ec3f1a4d1
captproton <carl@wdwhub.net>
2017-10-06 20:03:03 -0700

    FIXED: sleepy typing

--- spec/support/i18n.rb
+++ spec/support/i18n.rb
@@ -1,9 +1,3 @@
 RSpec.configure do |config|
   config.include ActionView::Helpers::TranslationHelper
-  en:
-  hotels:
-    index:
-      title: "WDW Hub|Walt Disney World Hotels"
-    show:
-      title: "Choose %{name}!"
 end
