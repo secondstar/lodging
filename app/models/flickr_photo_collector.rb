259311e975bafd933a5ffc222e7aa32eabcca961
captproton <carl@wdwhub.net>
2017-10-09 10:13:09 -0700

    FIXED: flickr photos were not being cached

--- app/models/flickr_photo_collector.rb
+++ app/models/flickr_photo_collector.rb
@@ -25,13 +25,7 @@ class FlickrPhotoCollector
 	def cache_photos(photo_collection)
 		photos = photo_collection.flatten
 		photos.each do |photo|
-		#move "id" to "flickr_id"
-		photo.flickr_id = photo.id.to_s
-		photo.delete_field("id")
-		# convert from 0/1 to boolean
-		photo.ispublic = to_boolean(photo.ispublic)
-		photo.isfriend = to_boolean(photo.isfriend)
-		photo.isfamily = to_boolean(photo.isfamily)
+			self.prepare_attributes_for_storage(photo)
 			hotel_photo = CachedFlickrPhoto.find_or_create_by(flickr_id: photo.flickr_id)
 			hotel_photo.update(photo.to_h)
 		end
@@ -42,9 +36,9 @@ class FlickrPhotoCollector
 		photo.flickr_id = photo.id.to_s
 		photo.delete_field("id")
 		# convert from 0/1 to boolean
-		photo.ispublic = to_boolean(photo.ispublic)
-		photo.isfriend = to_boolean(photo.isfriend)
-		photo.isfamily = to_boolean(photo.isfamily)
+		photo.ispublic = self.to_boolean(photo.ispublic)
+		photo.isfriend = self.to_boolean(photo.isfriend)
+		photo.isfamily = self.to_boolean(photo.isfamily)
 	end
 
 	def to_boolean(value)
