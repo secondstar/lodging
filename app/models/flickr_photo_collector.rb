cdff10a3de96069383195d7eafc686557c0e7343
captproton <carl@wdwhub.net>
2017-10-09 08:25:56 -0700

    well that didn't work

--- app/models/flickr_photo_collector.rb
+++ app/models/flickr_photo_collector.rb
@@ -25,7 +25,13 @@ class FlickrPhotoCollector
 	def cache_photos(photo_collection)
 		photos = photo_collection.flatten
 		photos.each do |photo|
-			prepare_attributes_for_storage(photo)
+		#move "id" to "flickr_id"
+		photo.flickr_id = photo.id.to_s
+		photo.delete_field("id")
+		# convert from 0/1 to boolean
+		photo.ispublic = to_boolean(photo.ispublic)
+		photo.isfriend = to_boolean(photo.isfriend)
+		photo.isfamily = to_boolean(photo.isfamily)
 			hotel_photo = CachedFlickrPhoto.find_or_create_by(flickr_id: photo.flickr_id)
 			hotel_photo.update(photo.to_h)
 		end
